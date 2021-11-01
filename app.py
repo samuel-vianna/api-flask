3cb93a16cfc0a0e328e1b66e610095e50b7e04c5
Samuel <samuel-vianna@hotmail.com>
2021-11-01 19:45:28 -0300

    voz

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
+from flask import request
 from flask_cors import CORS
+from playsound import playsound
+import gtts
 
 app = Flask(__name__)
 
@@ -10,10 +13,23 @@ cors = CORS(app, resource={r"/*":{"origins": "*"}})
 def index():
     return "<h1>FALA MANO </h1>"
 
+@app.route("/nome", methods=['GET'])
+def teste():
+    input = request.args.get('nome', default = 'sasa', type = str)
+    return '{}'.format(input.split(','))
+
+@app.route('/voz', methods=['GET'])
+def voz():
+    input = request.args.get('nome', default=' ', type=str)
+    teste = 'Olá, {}'.format(input)
+    frase = gtts.gTTS(teste, lang='pt-br')
+    frase.save('frase.mp3')
+    playsound('frase.mp3')
+    return "<h1>Olá, {} </h1>".format(input)
 
 def main():
     port = int(os.environ.get("PORT", 5000))
-    print(port)
+    print(port)   
     app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
